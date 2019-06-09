da8e7ab8ce7b8939cdc5f820bc8c316b65861c9a
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-06-09 10:50:54 -0300

    Comit inicial

---
+++ src/test/java/br/com/abgi/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abgi.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
