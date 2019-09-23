f14a360202bab5db6b71897064e2f68b390456c0
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-09-23 19:38:37 -0300

    adicionando internacionalizacao

--- src/main/java/br/com/abgi/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/br/com/abgi/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,14 +1,18 @@
 package br.com.abgi.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.abgi.restfulwebservices.helloworld.HelloWorldBean;
-
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	private String helloWorld = "Hello World!";
 
 	@GetMapping(path = "/hello-world")
@@ -24,5 +28,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
-	}		
+	}
+	
+	// INTERNACIONALIZACAO
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+	}
 }
