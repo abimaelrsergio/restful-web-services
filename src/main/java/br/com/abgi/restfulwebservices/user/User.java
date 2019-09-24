185092edfe788b8dcbfb09ecedfbed9bfe360d88
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-09-23 21:45:45 -0300

    Melhorando a documentação do Swagger com anotações customizadas

--- src/main/java/br/com/abgi/restfulwebservices/user/User.java
+++ src/main/java/br/com/abgi/restfulwebservices/user/User.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,13 +14,16 @@ import lombok.ToString;
 @ToString
 @Getter
 @Setter
+@ApiModel(description="Detalhes sobre o usuário")
 public class User {
 
 	private Integer id;
 
+	@ApiModelProperty(notes="O nome deve ter pelo menos dois caracteres")
 	@Size(min=2, message="O nome deve ter pelo menos dois caracteres")
 	private String name;
 
+	@ApiModelProperty(notes="A data de nascimento deve ser no passado")
 	@Past
 	private Date birthDate;
 
